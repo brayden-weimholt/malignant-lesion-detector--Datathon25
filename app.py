a8e58104b361cb61b60658794ce94785a6d6775a
Daanesh Bogale <dbogale@uci.edu>
2025-04-13 10:30:21 -0700

    last minute changes

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from PIL import Image
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision.models as models
+import pickle
 
 app = Flask(__name__)
 
-# Load model
-model = models.resnet18(pretrained=True)
-num_ftrs = model.fc.in_features
-model.fc = nn.Sequential(
-    nn.Linear(num_ftrs, 512),
-    nn.ReLU(),
-    nn.Dropout(0.7),
-    nn.Linear(512, 1)
-)
-model.load_state_dict(torch.load('finetuned_model.pth', map_location=torch.device('cpu')))
+# Load model using pickle
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 model.eval()
 
 def user_preprocess(img):
@@ -25,20 +18,31 @@ def user_preprocess(img):
     pixel_size = int(size_in_inches * 300)  # 300 DPI
 
     image = Image.open(img)
+
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
     image = image.resize((pixel_size, pixel_size), Image.LANCZOS)
+
     image_arr = np.array(image) / 255.0
-    image_arr = np.expand_dims(image_arr, axis=0)
-    image_tensor = torch.tensor(image_arr, dtype=torch.float32).permute(0, 3, 1, 2)
+
+    image_tensor = torch.tensor(image_arr, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+
     return image_tensor
 
 def predict_out(prediction):
-    sigmoid = nn.Sigmoid()
-    probabilities = sigmoid(prediction).detach().numpy()
-    benign_prob, malignant_prob = float(probabilities[0][0]), float(1 - probabilities[0][0])
-    if benign_prob > malignant_prob:
-        return {"class": "benign"}
-    else:
+    sigmoid_pred = torch.sigmoid(prediction)
+
+    predicted_class = int((sigmoid_pred > 0.5).detach().numpy()[0][0])
+    print("Raw logit:", prediction)
+    print("After sigmoid:", sigmoid_pred)
+    print("Predicted class (0 for malignant, 1 for benign):", predicted_class)
+    
+
+    if sigmoid_pred < 0.01: 
         return {"class": "malignant"}
+    else:
+        return {"class": "benign"}
 
 @app.route('/', methods=['GET', 'POST'])
 def user_upload():
